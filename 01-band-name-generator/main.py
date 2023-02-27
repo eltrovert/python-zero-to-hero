bef3184b91a9346467fb88bbc68158a62ed7eeaf
Muhammad Cholid Hidayatullah <cholid.hidayatullah@privyid.tech>
2023-02-27 21:03:28 +0700

    first project

---
+++ 01-band-name-generator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+print("____________________________________")
+
+town = input("Where are you born?\n")
+animal = input("What is your favorite animal?\n")
+
+print("Your band name should be" + "\n" + town + " " + animal)
